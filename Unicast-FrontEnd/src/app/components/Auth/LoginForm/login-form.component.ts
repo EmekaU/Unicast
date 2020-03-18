091f944e2d4cce01a5587a5ef10499604ec61d6b
Oluwaseyi Kareem <kareemseyi@gmail.com>
2020-03-18 16:32:26 -0600

    Added Form Controls to Login Form

--- Unicast-FrontEnd/src/app/components/Auth/LoginForm/login-form.component.ts
+++ Unicast-FrontEnd/src/app/components/Auth/LoginForm/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.loginForm = new FormGroup({
+      'username': new FormControl(null),
+      'password': new FormControl(null)
+    })
+  }
+  onSubmit(): void {
+    console.log(this.loginForm)
   }
 
 }
