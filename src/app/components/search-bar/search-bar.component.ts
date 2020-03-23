a4a71718d13024bb787a0d8fac8955180ffe1088
ValentineUmeozo <valentine.umeozo@fcl.crs>
2020-03-22 23:19:06 -0600

    Search bar enhancement

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +7,16 @@ import { Component, OnInit , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
-
+  list = [{"name": "A", "value": "Podcast"}, {"name": "B", "value": "Podcast"}, {"name": "C", "value": "User"}, {"name": "D", "value": "Podcast"}, {"name": "E", "value": "User"}, {"name": "F", "value": "Podcast"}];
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  search(f: NgForm){
+    console.log(f.value.query);
+    // Do something with f.value.query
   }
 
 }
