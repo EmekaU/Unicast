d72b274cf48b731ae475e46290ea9fed3d0f2395
Oluwaseyi Kareem <kareemseyi@gmail.com>
2020-04-08 12:19:07 -0600

    added custom sidebar components

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,15 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
-
-  openTab = 1;
-  
-  toggleTabs($tabNumber: number){
-    this.openTab = $tabNumber;
+    
   }
 
 }
