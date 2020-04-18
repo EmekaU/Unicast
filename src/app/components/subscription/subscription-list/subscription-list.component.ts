51984499bd38d902bf81ab8739fb47c5ccec930d
Valentine Umeozo <umeozoemeka@yahoo.com>
2020-04-18 02:34:14 -0600

    Final Commit

--- src/app/components/subscription/subscription-list/subscription-list.component.ts
+++ src/app/components/subscription/subscription-list/subscription-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { UserAPIService } from 'src/app/services/unicast-api.service';
 
 @Component({
   selector: 'app-subscription-list',
@@ -12,19 +13,26 @@ export class SubscriptionListComponent implements OnInit,OnDestroy {
  
   subscriptions: [] = null;
   subscription: Subscription;
+  user: {} = {};
 
-  constructor(private router: Router, private route: ActivatedRoute, private userService: UserService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private userService: UserService, private userApi: UserAPIService) { }
 
   ngOnInit(): void {
     this.subscription = this.userService.forwardSubscriptions.subscribe( (data: []) => {
       this.subscriptions = data;
       console.log(this.subscriptions);
     });
+
   }
 
   redirectToUserProfile(sub: Object){
-    console.log(sub);
-    this.router.navigate([`/user-profile/${sub["id"]["subscribeToId"]}`]);
+    this.subscription = this.userApi.getUser(sub["id"]["subscribeToId"]).subscribe(
+      userData => {
+        this.userService.forwardUser.next(userData);
+        this.router.navigateByUrl(`/user-profile/${sub["id"]["subscribeToId"]}`);
+      }
+    )
+
   }
 
   ngOnDestroy(){
